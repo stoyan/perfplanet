792341469238a1650c87f383213bf4e5480b7191
Stoyan <ssttoo@ymail.com>
2012-08-07 15:59:27 -0700

    EOT

--- createRss.php
+++ createRss.php
@@ -19,7 +19,7 @@ try {
     }
     $date = date("r"); // RFC_2822
 
-    $rss = <<<"EOT"
+    $rss = <<<EOT
 <?xml version="1.0" encoding="utf-8"?>
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
@@ -43,7 +43,7 @@ EOT;
         $rss .= "</item>\n";
     }
 
-    $rss .= <<<"EOT"
+    $rss .= <<<EOT
 </channel>
 </rss>
 EOT;
