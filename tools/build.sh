73221b278481bfb46403540b4e76357643eda1a6
Stoyan <ssttoo@ymail.com>
2016-12-01 01:30:38 -0800

    hello 2016

--- tools/build.sh
+++ tools/build.sh
@@ -1,2 +1,2 @@
-jsc builder.js -- curl > ../yql.txt
-jsc builder.js -- html "`cat ../perfplanet.html`" "`cat ../perfplanet.css`" "`cat ../perfplanet.js`" > ../index.html
+/System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Resources/jsc builder.js -- curl > ../yql.txt
+/System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Resources/jsc builder.js -- html "`cat ../perfplanet.html`" "`cat ../perfplanet.css`" "`cat ../perfplanet.js`" > ../index.html
